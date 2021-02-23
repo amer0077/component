9a03460dcc1f7319f7e581853c24652053010b70
amer0077 <amermajouli5@gmail.com>
2021-02-23 23:01:11 +0100

    component

---
+++ src/component/profile/FullName.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FullName = () => {
+    return (
+        <div>
+            <h1>Amer majouli  </h1>
+        </div>
+    );
+}
+
+export default FullName;
---
+++ src/component/profile/ProfilPhoto.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import image  from './image.jpg';
+
+
+
+
+const ProfilPhoto = () => {
+    return (
+        <div>
+            <img src = {image}  />
+        
+        </div>
+    );
+}
+
+export default ProfilPhoto;
